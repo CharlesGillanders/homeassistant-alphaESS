7b9e23b52e765e00aba3be69678335a470a4fbfc
CharlesGillanders <53312816+CharlesGillanders@users.noreply.github.com>
2021-08-17 15:47:17 +0100

    Fix for calculating Load instead of incorrectly reading it
    Load was being incorrectly read from sva in the returned statistics from AlphaESS.  Correct function looks to be to calculate it from adding other measurements.

--- alphaESS_test.py
+++ alphaESS_test.py
@@ -3,7 +3,7 @@ from alphaess_api import AlphaESSAPI
 import json
 import time
 
-AlphaESS = AlphaESSAPI("www.alphaess.com", "{INSERT_USERNAME}", "{INSERT_PASSWORD}")
+AlphaESS = AlphaESSAPI("www.alphaess.com", "charlesgillanders", "iNwkjCVdzfL6LiYFwC7")
 
 # print (AlphaESS._host)
 
@@ -25,30 +25,34 @@ if AlphaESS.connect():
                if "ppv1" in statistics:
                    if statistics["ppv1"] is not None:
                        ppv = ppv + statistics["ppv1"]
+                       load = load + statistics["ppv1"]
                if "ppv2" in statistics:
                    if statistics["ppv2"] is not None:
                        ppv = ppv + statistics["ppv2"]
+                       load = load + statistics["ppv2"]
                if "ppv3" in statistics:
                    if statistics["ppv3"] is not None:
                        ppv = ppv + statistics["ppv3"]
+                       load = load + statistics["ppv3"]
                if "soc" in statistics:
                    if statistics["soc"] is not None:
                        soc = statistics["soc"]
                if "pbat" in statistics:
                    if statistics["pbat"] is not None:
                        pbat = statistics["pbat"]
-               if "sva" in statistics:
-                   if statistics["sva"] is not None:
-                       load = statistics["sva"]
+                       load = load + statistics["pbat"]
                if "pmeter_l1" in statistics:
                    if statistics["pmeter_l1"] is not None:
                        grid = grid + statistics["pmeter_l1"]
+                       load = load + statistics["pmeter_l1"]
                if "pmeter_l2" in statistics:
                    if statistics["pmeter_l2"] is not None:
                        grid = grid + statistics["pmeter_l2"]
+                       load = load + statistics["pmeter_l2"]
                if "pmeter_l3" in statistics:
                    if statistics["pmeter_l3"] is not None:
                        grid = grid + statistics["pmeter_l3"]
+                       load = load + statistics["pmeter_l3"]
                print (f"power pv : {ppv} W, State of Charge : {soc} %, Battery : {pbat} W, Load :  {load} W, Grid : {grid} W")
 
                time.sleep(10)
