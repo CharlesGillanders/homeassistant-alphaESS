9abd81e771930e18daa0840148294117429c456a
Joshua Leaper <poshernater@outlook.com>
2025-03-02 15:13:25 +1030

    Add fallback if no currency exists via the app

--- custom_components/alphaess/sensor.py
+++ custom_components/alphaess/sensor.py
@@ -42,6 +42,9 @@ async def async_setup_entry(hass, entry, async_add_entities) -> None:
     for serial, data in coordinator.data.items():
         model = data.get("Model")
         currency = data.get("Currency")
+        if currency is None:
+            currency = hass.config.currency
+
         _LOGGER.info(f"New Inverter: Serial: {serial}, Model: {model}")
 
         # This is done due to the limited data that inverters like the Storion-S5 support
